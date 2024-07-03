3369ed2af92ed07c55c0aa096ffcd492796876bc
Jerzy Wilczek <jerzy.wilczek@swmansion.com>
2024-07-03 15:07:38 +0200

    Add a keybind for jumping to next error

--- dot-config/nvim/lua/custom/maps.lua
+++ dot-config/nvim/lua/custom/maps.lua
@@ -7,6 +7,14 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '[e', function ()
+  vim.diagnostic.goto_prev({ severity = "ERROR" })
+end)
+vim.keymap.set('n', ']e', function ()
+  vim.diagnostic.goto_next({ severity = "ERROR" })
+end)
+
+
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
