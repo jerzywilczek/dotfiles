4d395507f88aac42d4bf87e2f944a06c2f5f443c
Jerzy Wilczek <jerzykwilczek@gmail.com>
2024-07-04 02:07:52 +0200

    Add trouble symbols view and make trouble gain focus on launch

--- dot-config/nvim/lua/custom/plugins/trouble.lua
+++ dot-config/nvim/lua/custom/plugins/trouble.lua
@@ -6,8 +6,13 @@ return {
   keys = {
     {
       "<leader>tt",
-      "<cmd>Trouble diagnostics toggle<cr>",
+      "<cmd>Trouble diagnostics toggle focus=true<cr>",
       desc = "toggle [T]rouble (diagnostics)",
+    },
+    {
+      "<leader>ts",
+      "<cmd>Trouble symbols toggle focus=true<cr>",
+      desc = "toggle [T]rouble ([S]ymbols)",
     },
   }
 }
