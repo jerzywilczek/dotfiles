cd95455cf0027c892588d928fda19b7169feda38
Jerzy Wilczek <jerzy.wilczek@swmansion.com>
2025-07-01 15:32:21 +0200

    Change Oil to the default file explorer, cleanup unused things

--- dot-config/nvim/lua/custom/plugins/oil.lua
+++ dot-config/nvim/lua/custom/plugins/oil.lua
@@ -5,4 +5,10 @@ return {
   },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+
+  config = function ()
+    require("oil").setup({ default_file_explorer = true })
+
+    vim.keymap.set('n', '<leader>f', '<CMD>Oil<CR>', { desc = "Open [F]ile explorer" })
+  end
 }
