bcf56c4739992e0ac393f6d69507fcba60dfed1a
Jerzy Wilczek <jerzykwilczek@gmail.com>
2025-12-05 11:13:39 +0100

    Various fixes
    - add a profiler
    - add a delay on which-key
    - turn off update checks

--- dot-config/nvim/lua/custom/plugins/which_key.lua
+++ dot-config/nvim/lua/custom/plugins/which_key.lua
@@ -4,7 +4,7 @@ return {              -- Useful plugin to show you pending keybinds.
   opts = {
     -- delay between pressing a key and opening which-key (milliseconds)
     -- this setting is independent of vim.o.timeoutlen
-    delay = 0,
+    delay = 200,
     -- Document existing key chains
     spec = {
       { '<leader>c', group = '[C]ode'},
